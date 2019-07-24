0d7fbc73fe8a8c0e44f71d6f398fee0fe1abc09c
Berlin Smith <berlin.smithjr@gmail.com>
2019-07-23 21:49:58 -0400

    added snake class

--- index.js
+++ index.js
@@ -11,6 +11,44 @@ class GameBoard {
 
 }
 
+class Snake {
+    constructor(gameWidth, gameHeight) {
+        this.gameWidth = gameWidth;
+        this.size = 20;
+        this.bodyLength = 1;
+        //this.maxSpeed = 5;
+        //this.speed = { x: 5, y: 0};
+        this.position = [
+            {
+                x: 260,
+                y: 300,
+            }
+        ]
+    }
+
+    draw(ctx) {
+        ctx.fillStyle = 'black';
+        for (let i = 0; i < this.bodyLength; i++) { 
+            if (i !== 0) {
+                this.position.push(
+                    {
+                        x: this.position[0].x - (i * this.size),
+                        y: this.position[0].y
+                    }
+                );
+             }else{
+                 this.position = [
+                     {
+                        x: this.position[i].x,
+                        y: this.position[i].y
+                     }
+                 ]
+             }
+
+             ctx.fillRect(this.position[i].x, this.position[i].y, this.size, this.size);
+        }
+    }
+}
 
 let canvas = document.getElementById('game-screen');
 let context = canvas.getContext('2d');
@@ -19,12 +57,15 @@ const GAME_WIDTH = 800;
 const GAME_HEIGHT = 540;
 
 let gameBoard = new GameBoard(GAME_WIDTH, GAME_HEIGHT);
+let snake = new Snake(GAME_WIDTH, GAME_HEIGHT);
+
 
 window.onload = () => {
     console.log('ready');
 
     drawGameTitleArea();
     gameBoard.draw(context);
+    snake.draw(context);
 
 }
 
