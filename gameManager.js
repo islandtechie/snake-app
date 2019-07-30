8fd02ed3a1fce5575837cdd507bfadb2cff60d6d
Berlin Smith <berlin.smithjr@gmail.com>
2019-07-29 22:19:59 -0400

    Refactored Snake Class
    recreated snake class to only hold snake information. Also integrated snake class into Game manager

--- gameManager.js
+++ gameManager.js
@@ -1,9 +1,18 @@
 class GameManager {
-    constructor(gameBoard) {
-        this.gameBoard = gameBoard;
+    constructor(gameBoardWidth, gameBoardheight) {
+        this.gameBoard = new GameBoard(gameBoardWidth, gameBoardheight);
+        this.snake = new Snake();
     }
 
     initializeGame() {
         this.gameBoard.draw();
+        this.drawSnake();
+    }
+
+    drawSnake() {
+        this.gameBoard.ctx.fillStyle = 'black';
+        for (let i = 0; i < this.snake.position.length; i++) { 
+            this.gameBoard.ctx.fillRect(this.snake.position[i].x, this.snake.position[i].y, this.snake.size, this.snake.size);
+        }
     }
 }
--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const GAMEBOARD_WIDTH = 800;
 const GAMEBOARD_HEIGHT = 540;
-const gameBoard = new GameBoard(GAMEBOARD_WIDTH, GAMEBOARD_HEIGHT);
 
-const gameManager = new GameManager(gameBoard);
+const gameManager = new GameManager(GAMEBOARD_WIDTH, GAMEBOARD_HEIGHT);
 
 gameManager.initializeGame();
