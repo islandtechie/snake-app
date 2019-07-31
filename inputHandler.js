c365331a54c84a561988802f2543fbd55f1a2595
Berlin Smith <berlin.smithjr@gmail.com>
2019-07-31 19:28:00 -0400

    correct two way direction issue
    snake can only move in a forward direction. If it is going right on the horizontal, it can only move up or down and not left (in the opposite direction

--- inputHandler.js
+++ inputHandler.js
@@ -3,15 +3,19 @@ class InputHandler {
         document.addEventListener('keydown', (e) => {
             switch(e.keyCode) {
                 case 37:
+                    if (snake.direction === 'RIGHT') {return;}
                     snake.direction = 'LEFT';
                     break;
                 case 38:
+                    if (snake.direction === 'DOWN') {return;}
                     snake.direction = 'UP';
                     break;
                 case 39:
+                    if (snake.direction === 'LEFT') {return;}
                     snake.direction = 'RIGHT';
                     break;
                 case 40:
+                    if (snake.direction === 'UP') {return;}
                     snake.direction = 'DOWN';
                     break;
             }
